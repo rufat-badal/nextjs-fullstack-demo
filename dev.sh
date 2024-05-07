e23be4d891a1f0a48c548ad5a4958366b0642570
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-05-07 20:26:28 +0700

    Run my-next-app inside its own docker container

---
+++ dev.sh
@@ -0,0 +1,15 @@
+# Stop all running containers
+docker kill $(docker ps -aq) && docker rm $(docker ps -aq)
+
+# Add this once we need 2 services to comminicate with each other
+# Create a network, which allows containers to communicate
+# with each other, by using their container name as a hostname
+# docker network create my-network
+
+# Build dev
+docker compose --env-file .env --env-file .secret.env -f docker-compose.dev.yml build
+
+# Up dev
+# --renew-anon-volumes
+#   postgres/mysql retrieve volumes from previous containers after being killed
+docker compose --env-file .env --env-file .secret.env -f docker-compose.dev.yml up --renew-anon-volumes
