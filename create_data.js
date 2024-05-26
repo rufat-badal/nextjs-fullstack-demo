0b5487a93fc0264db424956ea1b8d2d91b22e38f
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-05-26 16:56:59 +0200

    Created users table

---
+++ create_data.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const seedDataFile = "./postgres/data.sql";
+
+const uuidExtensionData = `CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+const usersTableData = `CREATE TABLE IF NOT EXISTS users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);`;
+
+function appendData(data) {
+    fs.writeFileSync(seedDataFile, data, { flag: "a" });
+}
+
+function createFile() {
+    fs.writeFileSync(seedDataFile, uuidExtensionData + "\n");
+}
+
+function main() {
+    createFile();
+    appendData("\n" + usersTableData + "\n");
+}
+
+main();
---
+++ postgres/data.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
