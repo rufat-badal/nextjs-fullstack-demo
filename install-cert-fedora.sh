84bbff11fda35093125ca655b3b3a526aede5f6a
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-05-27 22:24:34 +0200

    Provide env file

--- install-cert-fedora.sh
+++ install-cert-fedora.sh
@@ -1,3 +1,3 @@
-docker compose -f docker-compose.dev.yml cp caddy:/data/caddy/pki/authorities/local/root.crt /tmp/root.crt
+docker compose -f docker-compose.dev.yml --env-file .env.local cp caddy:/data/caddy/pki/authorities/local/root.crt /tmp/root.crt
 sudo trust anchor --store /tmp/root.crt
 sudo update-ca-trust
--- install-cert-macos.sh
+++ install-cert-macos.sh
@@ -1,2 +1,2 @@
-docker compose -f docker-compose.dev.yml cp caddy:/data/caddy/pki/authorities/local/root.crt /tmp/root.crt
+docker compose -f docker-compose.dev.yml --env-file .env.local cp caddy:/data/caddy/pki/authorities/local/root.crt /tmp/root.crt
 security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain /tmp/root.crt
