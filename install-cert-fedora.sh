ccfb88ffede9b08f3021b7bcf8114aef0ed521c3
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-05-31 21:37:22 +0200

    Remove old cert files

--- install-cert-fedora.sh
+++ install-cert-fedora.sh
@@ -1,3 +1,5 @@
-docker compose -f docker-compose.dev.yml --env-file .env.local cp caddy:/data/caddy/pki/authorities/local/root.crt /tmp/root.crt
+# Remove old certificates
+sudo rm /etc/pki/ca-trust/source/Caddy_Local_Authority*.p11-kit
+docker compose -f docker-compose.dev.yml --env-file .env.local --env-file .env cp caddy:/data/caddy/pki/authorities/local/root.crt /tmp/root.crt
 sudo trust anchor --store /tmp/root.crt
 sudo update-ca-trust
