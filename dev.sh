802bb765c8ccb1a4742a480d6cbfe0584674f985
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-06-01 12:53:35 +0200

    Improved the docker script

--- dev.sh
+++ dev.sh
@@ -1,18 +1,33 @@
+DOCKER_NETWORK=my-network
+
 # Stop all running containers
-docker kill $(docker ps -aq) && docker rm $(docker ps -aq)
+# we need to surround $running_containers with "" in order to assure that the string
+# is not split up in case of multiple containers
+running_containers=$(docker ps -q)
+if [ ! -z "$running_containers" ]; then
+    docker kill $running_containers
+fi
 
-# Create a network, which allows containers to communicate
-# with each other, by using their container name as a hostname
-docker network create my-network
+# Remove ALL old containers
+containers=$(docker ps -aq)
+if [ ! -z "$containers" ]; then
+    docker rm $containers
+fi
 
-# --renew-anon-volumes does not work with the postgres container
-# the command below will delete the anonymous volume of the postgres container
-docker compose --env-file .env.local --env-file .env -f docker-compose.dev.yml rm -fv postgres
+# Remove all unused anonymous docker volumes
+# In our case this makes sure that the anonymous postgres volume from
+# a previous run is deleted
+docker volume prune -f
+
+# Only create docker network if it does not exist
+if [ ! "$(docker network ls | grep $DOCKER_NETWORK)" ]; then
+    docker network create $DOCKER_NETWORK
+fi
 
 # Build dev
 docker compose --env-file .env.local --env-file .env -f docker-compose.dev.yml build
 
-# Up dev
-# --renew-anon-volumes
-#   postgres/mysql retrieve volumes from previous containers after being killed
+# # Up dev
+# # --renew-anon-volumes
+# #   postgres/mysql retrieve volumes from previous containers after being killed
 docker compose --env-file .env.local --env-file .env -f docker-compose.dev.yml up --renew-anon-volumes
