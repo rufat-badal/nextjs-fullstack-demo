fa7f2ae8ba4adbc3e5d6401e5b601c1746ae450c
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-06-02 12:13:42 +0200

    Added table types

--- nextjs-dashboard/app/lib/types.ts
+++ nextjs-dashboard/app/lib/types.ts
@@ -1,3 +1,7 @@
+import { Generated } from 'kysely';
+
+type UUID = `${string}-${string}-${string}-${string}-${string}`;
+
 export interface Database {
   users: UsersTable;
   invoices: InvoicesTable;
@@ -5,10 +9,29 @@ export interface Database {
   revenue: RevenueTable;
 }
 
-export interface UsersTable {}
+export interface UsersTable {
+  id: Generated<UUID>;
+  name: string;
+  email: string;
+  password: string;
+}
 
-export interface InvoicesTable {}
+export interface InvoicesTable {
+  id: Generated<UUID>;
+  customer_id: UUID;
+  amount: number;
+  status: 'pending' | 'paid';
+  date: `${number}-${number}-${number}`;
+}
 
-export interface CustomersTable {}
+export interface CustomersTable {
+  id: Generated<UUID>;
+  name: string;
+  email: string;
+  image_url: string;
+}
 
-export interface RevenueTable {}
+export interface RevenueTable {
+  month: string;
+  revenue: number;
+}
